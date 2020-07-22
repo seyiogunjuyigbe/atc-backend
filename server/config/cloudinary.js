6069213e6e1786bdf4af79e5ada08518af81e327
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-07-22 21:24:52 +0100

    multer-storage

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,24 @@
-const { config, uploader } = require('cloudinary').v2;
-const {cloud_name,api_key,api_secret} = require('./local')
-const cloudinaryConfig = () => config({
-cloud_name,
-api_key,
-api_secret,
+// const { config, uploader } = require('cloudinary').v2;
+// const {cloud_name,api_key,api_secret} = require('./local')
+// const cloudinaryConfig = () => config({
+// cloud_name,
+// api_key,
+// api_secret,
+// });
+// cloudinaryConfig()
+// module.exports = { uploader };
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+ 
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'some-folder-name',
+    format: async (req, file) => 'png', // supports promises as well
+    public_id: (req, file) => 'computed-filename-using-request',
+  },
 });
-cloudinaryConfig()
-module.exports = { uploader };
+ 
+const parser = multer({ storage: storage });
+module.exports = parser;
