d57f7db112eea193f13dc2b9a53520f89a63f8f4
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-07-22 15:08:02 +0100

    setup

---
+++ server/controllers/contentController.js
@@ -0,0 +1,10 @@
+const models = require('../models');
+const responses = require("../helper/responses");
+const uuidv1 = require("uuid/v1");
+const { check, validationResult } = require('express-validator');
+module.exports+{
+    async createContent(req,res){
+        const {} = req.body;
+
+    }
+}
--- server/models/content.js
+++ server/models/content.js
@@ -1,10 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const content = sequelize.define('content', {
-   
+    id : {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+  },
+  type:{
+    type: DataTypes.ENUM('video','image','gif'),
+  },
+  forType: DataTypes.STRING,
+  forId:DataTypes.INTEGER,
+  createdAt:{
+    type:DataTypes.DATE,
+    default: Date.now()
+  }  ,
+  updatedAt: DataTypes.DATE,
+  deletedAt: DataTypes.DATE
   }, {});
   content.associate = function(models) {
     // associations can be defined here
+
   };
   return content;
 };
