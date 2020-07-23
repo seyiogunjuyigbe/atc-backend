502641451aa171e917e6139310e3c54e32d0ba99
Awotundun Afeez <Awotundun@gmail.com>
2020-07-23 13:46:28 +0100

    product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -19,29 +19,29 @@ module.exports = {
 
     try {
      
-           const Subscription = await models.subscriptions.create(req.body);
-                    if (Subscription) {
+           const Product = await models.products.create(req.body);
+                    if (Product) {
                     
-                      return res.status(200).send(responses.success(200, "Your Subscription was successfully created.",Subscription));
+                      return res.status(200).send(responses.success(200, "Your Product was successfully created.",Product));
                     } else {
                       return res
                         .status(400)
-                        .send(responses.error(400, "Unable to create Subscription"));
+                        .send(responses.error(400, "Unable to create Product"));
                     }
 
         } catch (error) {
         return res
             .status(500)
             .send(
-            responses.error(500, `Error creating a Subscription ${error.message}`)
+            responses.error(500, `Error creating a Product ${error.message}`)
             );
         }
     },
-    viewSubscription: async(res,req) => {
+    viewProduct: async(res,req) => {
       try {
-        const Subscription =  models.subscriptions.findByPk(req.params.id);
-        if (!Subscription ) {
-          return res.status(400).send(responses.error(400, "Subscription not found"));
+        const Product =  models.products.findByPk(req.params.id);
+        if (!Product ) {
+          return res.status(400).send(responses.error(400, "Product not found"));
         } else {
           return res
             .status(200)
@@ -49,18 +49,18 @@ module.exports = {
               responses.success(
                 200,
                 "Record was retreived successfully",
-                Subscription
+                Product
               )
             );
         }
       } catch (error) {
         return res
           .status(500)
-          .send(responses.error(500, `Error viewing a subscription ${error.message}`));
+          .send(responses.error(500, `Error viewing a product ${error.message}`));
       }
 
     },
-    listSubscription: (res,req) => {
+    listProduct: (res,req) => {
        
       var offset = (req.query.offset) ? req.query.offset : 0;
       var limit = (req.query.limit) ? req.query.limit : 20;
@@ -72,14 +72,14 @@ module.exports = {
   
    
       
-      models.subscriptions.findAndCountAll({
+      models.products.findAndCountAll({
           offset: parseInt(offset), 
           limit: parseInt(limit),
           order: ordering,
          
-      }).then(function(subscription){
+      }).then(function(product){
         
-          return res.status(200).send(responses.success(200,"Record was retreived successfully",subscription));
+          return res.status(200).send(responses.success(200,"Record was retreived successfully",product));
       })
 
 
@@ -87,17 +87,17 @@ module.exports = {
     
 
     },
-    updateSubscription:async (res,req) => {
+    updateProduct:async (res,req) => {
      
       try {
-        const result = await models.subscriptions.update(
+        const result = await models.products.update(
           req.body,
           { where: { _id: req.params.id} }
         );
       
         return res
           .status(200)
-          .send(responses.success(200, "Membership was updated successfully", result));
+          .send(responses.success(200, "Product was updated successfully", result));
       } catch (err) {
         return res
         .status(500)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const indexRoute = express.Router();
 const authRoute = require("./authRoute");
-const membershipRoute = require("./membershipRoute");
-
+const subRoute = require("./subscriptionRoute");
+const membershipRoute =  require('./membershipRoute');
+const productRoute = require('./productRoute');
 
 indexRoute.use("/", authRoute);
 indexRoute.use("/",membershipRoute);
+indexRoute.use("/",subRoute);
+indexRoute.use("/",productRoute);
 
 
 module.exports = indexRoute;
