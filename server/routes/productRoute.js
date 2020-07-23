f946766e7aeb2a363645af41bf2cbfab5506c9c2
Awotundun Afeez <Awotundun@gmail.com>
2020-07-23 18:43:21 +0100

    error

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -4,12 +4,12 @@ const productRoute = express.Router();
 
 
 
-productRoute.post("/ product/create",  productCrl.create);
+productRoute.post("/product/create",  productCrl.create);
 
 
-productRoute.get("/ product/list",  productCrl.listProduct);
-productRoute.put("/ product/update/:id",  productCrl.updateProduct);
-productRoute.get("/ product/one/:id",  productCrl.viewProduct);
+productRoute.get("/product/list",  productCrl.listProduct);
+productRoute.put("/product/update/:id",  productCrl.updateProduct);
+productRoute.get("/product/one/:id",  productCrl.viewProduct);
 
 
 
