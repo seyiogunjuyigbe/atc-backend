5918bd9673bfe6f4703c1f4c9538696f7b893eb1
Awotundun Afeez <Awotundun@gmail.com>
2020-07-23 16:47:12 +0100

    pull request fix

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-const express = require("express");
-const cors = require("cors");
-const cron = require("node-cron");
+const express = require('express');
+const cors = require('cors');
+const cron = require('node-cron');
 const morgan = require('morgan');
 const { safeLoad } = require('js-yaml');
 const { readFileSync } = require('fs');
@@ -12,17 +12,18 @@ const ejs = require('ejs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 const { loadDefinitions, loadPaths } = require('../documentations');
-
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json({ limit: '5mb', type: 'application/json' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
   next();
 });
 //log every request to the database
@@ -31,14 +32,17 @@ app.use(morgan('dev'));
 app.set('views', path.join(__dirname, '../public'));
 app.set('view engine', 'pug');
 
-app.use("/api/v1/", require("./routes/index"));
+app.use('/api/v1/', require('./routes/index'));
 
 app.get('/swagger.json', (req, res) => {
   const data = {
     paths: loadPaths() || '',
     definitions: loadDefinitions() || '',
   };
-  const swaggerTemplate = readFileSync(path.join(__dirname, '../documentations/swagger.yaml'), 'utf8');
+  const swaggerTemplate = readFileSync(
+    path.join(__dirname, '../documentations/swagger.yaml'),
+    'utf8',
+  );
   const swaggerSchema = ejs.render(swaggerTemplate, data);
 
   res.setHeader('Content-Type', 'application/json');
@@ -50,7 +54,7 @@ app.get('/', (req, res) => res.render('index'));
 //error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something went wrong!");
+  res.status(500).send('Something went wrong!');
 });
 
 app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,68 +1,72 @@
 require('dotenv').config();
-const nodemailer = require("nodemailer");
-const sgTransport = require("nodemailer-sendgrid-transport");
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 const handlebars = require('handlebars');
 const fs = require('fs');
 const DEFAULT_EMAIL_SENDER = process.env.DEFAULT_EMAIL_SENDER;
 const SMTP_USERNAME = process.env.SENDGRID_USER;
-const  SMTP_PASSWORD = process.env.SENDGRID_PASSWORD;
+const SMTP_PASSWORD = process.env.SENDGRID_PASSWORD;
 
 let MailController = {};
 
-let readHTMLFile = function(path, callback) {
-  fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
-      if (err) {
-          throw err;
-          callback(err);
-      }
-      else {
-          callback(null, html);
-      }
+let readHTMLFile = function (path, callback) {
+  fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+    if (err) {
+      throw err;
+      callback(err);
+    } else {
+      callback(null, html);
+    }
   });
 };
 
-MailController.sendTemplatedMail = async function(templateName = null, data = {}, recipient = null,subject = null){
-    
+MailController.sendTemplatedMail = async function (
+  templateName = null,
+  data = {},
+  recipient = null,
+  subject = null,
+) {
   const options = {
     auth: {
-      api_user:process.env.SENDGRID_USER,
-      api_key: process.env.SENDGRID_PASSWORD
-    }
+      api_user: process.env.SENDGRID_USER,
+      api_key: process.env.SENDGRID_PASSWORD,
+    },
   };
-    
-    readHTMLFile('public/emailtemplates/header.html', function(err, headerHtml){
-        let header = headerHtml;
 
-        readHTMLFile('public/emailtemplates/footer.html', function(err, footerHtml){
-            let footer = footerHtml;
+  readHTMLFile('public/emailtemplates/header.html', function (err, headerHtml) {
+    let header = headerHtml;
 
-            readHTMLFile(`public/emailtemplates/${templateName}`, async function(err, html) {
-                let template = handlebars.compile(header + html + footer);
-                let replacements = data;
-                let htmlToSend = template(replacements);
-                const mailer = nodemailer.createTransport(sgTransport(options));
-                let mailOptions = {
-                    from: `African Travel Club <${DEFAULT_EMAIL_SENDER}>`,
-                    to : recipient,
-                    subject : subject,
-                    html : htmlToSend
-                };
+    readHTMLFile('public/emailtemplates/footer.html', function (
+      err,
+      footerHtml,
+    ) {
+      let footer = footerHtml;
 
-                //NEW WAY VIA SENDGRID
-                await mailer.sendMail(mailOptions, (err, response) => {
-                  if (err) {
-                    return err;
-                  } else {
-                    return response;
-                  }
-                });
+      readHTMLFile(`public/emailtemplates/${templateName}`, async function (
+        err,
+        html,
+      ) {
+        let template = handlebars.compile(header + html + footer);
+        let replacements = data;
+        let htmlToSend = template(replacements);
+        const mailer = nodemailer.createTransport(sgTransport(options));
+        let mailOptions = {
+          from: `African Travel Club <${DEFAULT_EMAIL_SENDER}>`,
+          to: recipient,
+          subject: subject,
+          html: htmlToSend,
+        };
 
-
-               
-
-
-            });
+        //NEW WAY VIA SENDGRID
+        await mailer.sendMail(mailOptions, (err, response) => {
+          if (err) {
+            return err;
+          } else {
+            return response;
+          }
         });
+      });
     });
-}
+  });
+};
 module.exports = MailController;
