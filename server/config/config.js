fe8db25f2f256fce142e47848f34dcc83cdb1039
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-07-24 13:41:21 +0100

    ENV

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: null,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'
