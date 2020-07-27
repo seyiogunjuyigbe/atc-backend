06021e1928b714fef52be239b0193080518698b1
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-07-27 11:47:46 +0100

    routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const indexRoute = express.Router();
 const authRoute = require("./authRoute");
 const subRoute = require("./subscriptionRoute");
-const membershipRoute =  require('./membershipRoute');
+const membershipRoute = require('./membershipRoute');
 const productRoute = require('./productRoute');
-const contentRoutes = require('./contentRoute')
+const contentRoutes = require('./contentRoute');
+const activityRoute = require('./activityRoute')
 
 indexRoute.use("/auth", authRoute);
-indexRoute.use("/memeberships",membershipRoute);
-indexRoute.use("/subscriptions",subRoute);
-indexRoute.use("/products",productRoute);
+indexRoute.use("/memeberships", membershipRoute);
+indexRoute.use("/subscriptions", subRoute);
+indexRoute.use("/products", productRoute);
 indexRoute.use('/content', contentRoutes);
+indexRoute.use('/activities', activityRoute)
 
 
 module.exports = indexRoute;
