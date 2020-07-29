2e0f5a6bae8ff17d39ffcf1cbe58d9331a1f0d7d
Ridwan Taiwo <donriddo@gmail.com>
2020-07-29 01:34:11 +0100

    change Content model forId attribute to contentFor and add a discriminator on it

--- server/models/content.js
+++ server/models/content.js
@@ -1,28 +1,29 @@
 'use strict';
-const mongoose = require( 'mongoose' );
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ContentSchema = new Schema( {
-    type : {
-      type : String ,
-      enum : [ 'video' , 'image' , 'gif' ]
-    } ,
-    forType : {
-      type : String
-    } ,
-    forId : {
-      type : mongoose.Schema.Types.ObjectId
-    } ,
-    url : {
-      type : String
-    }
-  } ,
+const ContentSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['video', 'image', 'gif']
+  },
+  forType: {
+    type: String
+  },
+  contentFor: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: 'forType',
+  },
+  url: {
+    type: String
+  }
+},
   {
-    timestamps : true
+    timestamps: true
   }
 );
-const Content = mongoose.model( 'Content' , ContentSchema );
+const Content = mongoose.model('Content', ContentSchema);
 
 module.exports = Content;
 
