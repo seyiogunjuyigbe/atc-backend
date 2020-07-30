8f4f05e2719dee4cd2b235fb7a01cf7dd27615f9
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-07-30 23:12:53 +0100

    docs

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,6 +2,7 @@ const {
     STRIPE_SECRET_KEY, WEBHOOK_SECRET
 } = process.env
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
+const Transaction = require('../models/transaction')
 const {
     success,
     error
@@ -12,19 +13,23 @@ module.exports = {
         const sig = req.headers['stripe-signature'];
         try {
             let event = stripe.webhooks.constructEvent(req.body, sig, WEBHOOK_SECRET);
-
+            let currentTransaction = await Transaction.findOne({ stripePaymentId: event.data.object.id })
             let intent;
             if (event['type'] === 'payment_intent.succeeded') {
                 intent = event.data.object;
+                currentTransaction.set({ status: "successful" });
+                await currentTransaction.save()
                 return success(res, 200, { success: true, intent: intent.id });
             } else if (event['type'] === 'payment_intent.payment_failed') {
                 intent = event.data.object;
                 const message = intent.last_payment_error && intent.last_payment_error.message;
+                currentTransaction.set({ status: "failed" });
+                await currentTransaction.save()
                 return error({ success: false, intent: intent.id, message });
             }
 
         } catch (err) {
-            return error(res, 400, 'Invalid signature')
+            return error(res, 401, 'Invalid signature')
         }
 
     }
--- server/routes/paymentRoute.js
+++ server/routes/paymentRoute.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
 const { webhook } = require('../controllers/paymentController');
 const authenticate = require('../middlewares/authentication')
-router.post('/payment/webhook', authenticate, webhook)
+router.post('/webhook', authenticate, webhook);
+module.exports = router;
