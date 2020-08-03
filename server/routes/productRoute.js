7436597ce0fb9843140150c296b8505ef416768a
Ridwan Taiwo <donriddo@gmail.com>
2020-08-03 15:59:23 +0100

    authenticate post, put, purchase product endpoints

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -2,16 +2,18 @@ const express = require("express");
 const productCrl = require("../controllers/productController");
 const productRoute = express.Router();
 
+const authenticate = require('../middlewares/authentication')
 
 
-productRoute.post("/", productCrl.create);
+
+productRoute.post("/", authenticate, productCrl.create);
 
 
 productRoute.get("/", productCrl.listProduct);
-productRoute.put("/:productId", productCrl.updateProduct);
+productRoute.put("/:productId", authenticate, productCrl.updateProduct);
 productRoute.get("/:productId", productCrl.viewProduct);
 //productRoute.delete("/product/:productId",  productCrl.deleteProduct);
-productRoute.post("/:productId/purchase", productCrl.purchaseProduct);
+productRoute.post("/:productId/purchase", authenticate, productCrl.purchaseProduct);
 
 
 
