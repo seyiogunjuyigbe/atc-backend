7b2f8ddac24c5ad572bbe744346df63a1bb03ea8
Ridwan Taiwo <donriddo@gmail.com>
2020-08-03 16:22:30 +0100

    add extra fields to products and update swagger docs

--- server/models/product.js
+++ server/models/product.js
@@ -1,41 +1,54 @@
 'use strict';
-const mongoose = require( 'mongoose' );
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema( {
-    name : {
-      type : String
-    } ,
-    productCategory : {
-      type : mongoose.Schema.ObjectId
-    } ,
-    isMainProduct : {
-      type : Boolean ,
-      default: false,
-    } ,
-    imageUrl : {
-      type : String
-    } ,
-    shortName : {
-      type : String
-    } ,
-    packageID : {
-      type : mongoose.Schema.ObjectId ,
-      required : true
-    } ,
-    owner : {
-      type : mongoose.Schema.Types.ObjectId
-    } ,
-    description : {
-      type : String
-    }
-  } ,
+const ProductSchema = new Schema({
+  name: {
+    type: String
+  },
+  type: {
+    type: mongoose.Schema.ObjectId,
+    enum: ['main', 'upsell', 'downsell', 'order-bump', 'cross-sell'],
+  },
+  isMainProduct: {
+    type: Boolean,
+    default: false,
+  },
+  hasAirportTransfer: {
+    type: Boolean,
+    default: false,
+  },
+  imageUrl: {
+    type: String
+  },
+  defaultUrgency: {
+    type: Number,
+  },
+  shortName: {
+    type: String
+  },
+  packageID: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Package',
+    required: true
+  },
+  cancellationDaysLimit: {
+    type: Number,
+  },
+  activities: [{ type: Schema.Types.ObjectId, ref: 'Activity' }],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId
+  },
+  description: {
+    type: String
+  }
+},
   {
-    timestamps : true
+    timestamps: true
   }
 );
-const Product = mongoose.model( 'Product' , ProductSchema );
+const Product = mongoose.model('Product', ProductSchema);
 
 module.exports = Product;
 
