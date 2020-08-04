404cf539c733abbb5ac4e623c600f99badb837fd
Lawrence Nwoko <nwokolawrence6@gmail.com>
2020-08-04 22:02:51 +0100

    fixed bugs date issu on product

---
+++ cron.js
@@ -0,0 +1,26 @@
+const Product = require('./server/models/product')
+const moment = require('moment');
+module.exports = class Cron {
+
+   async productCron() {
+       console.log('Started Cron for Product')
+        const allProducts = await Product.find({});
+        for (let product = 0; product < allProducts.length; product++) {
+            const data = allProducts[product];
+            if(moment(new Date()).isSame(data.endDate, 'day')) {
+                data.statues = "expired"
+                await product.save()
+            }
+            if(moment(new Date()).isAfter(data.endDate)) {
+                data.statues = "wating"
+                await product.save();
+            }
+            if(moment(data.endDate).isAfter( moment(data.endDate, "DD-MM-YYYY" ).add( data.cycleNumber , 'days' ))) {
+                data.statues = "active"
+                data.endDate = moment( new Date() , "DD-MM-YYYY" ).add( data.cycleNumber , 'days' )
+                data.startDate = new Date();
+                await product.save()
+            }
+        }
+    } 
+}
