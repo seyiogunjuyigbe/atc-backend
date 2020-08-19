00f21e0db2ac03aa21c9f2c5e19b4b5274a882b8
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-08-19 16:08:56 +0100

    docs

--- server/controllers/walletController.js
+++ server/controllers/walletController.js
@@ -1,18 +1,19 @@
 const walletService = require("../services/walletService");
 const { success, error } = require("../middlewares/response");
-const { WalletHistory } = require("../models")
+const { WalletHistory } = require("../models");
+const QueryService = require("../services/queryService")
 module.exports = {
     async fetchUserWallet(req, res) {
         try {
             let userWallet = await walletService.fetchWallet(req.user.id);
-            if (typeOf(userWallet) !== "string") return success(res, 200, userWallet)
+            if (typeof (userWallet) !== "string") return success(res, 200, userWallet)
         } catch (err) {
             return error(res, 500, err.message)
         }
     },
     async fetchUserWalletHistory(req, res) {
         try {
-            let walletHistory = await WalletHistory.find({ user: req.params.userId });
+            let walletHistory = await QueryService.find(WalletHistory, req)
             return success(res, 200, walletHistory)
         } catch (err) {
             return error(res, 500, err.message)
@@ -20,7 +21,7 @@ module.exports = {
     },
     async fetchAllWalletHistories(req, res) {
         try {
-            let walletHistory = await WalletHistory.find({});
+            let walletHistory = await QueryService.find(WalletHistory, req)
             return success(res, 200, walletHistory)
         } catch (err) {
             return error(res, 500, err.message)
